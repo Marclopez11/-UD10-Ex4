815c466fcca5b534a3a60ce86811442fcd0eb21b
Marc Lopez <marclopezmarco469@gmail.com>
2022-04-24 00:47:59 +0200

    Ejercicio 4 sin terminar

---
+++ ud10ej4/src/ud10ej4/CalculoSimple.java
@@ -0,0 +1,72 @@
+package ud10ej4;
+
+public class CalculoSimple {
+	
+	
+	
+	public static double sumar(double x1, double x2) {
+		
+		double s = x1 + x2;
+		
+		return s ;
+		
+	}
+	
+	
+public static double resta(double x1, double x2) {
+		
+	double s = x1 + x2;
+		
+		return s ;
+		
+	}
+
+
+public static double multi(double x1, double x2) {
+	
+	double s = x1 * x2;
+	
+	return s ;
+	
+}
+
+
+public static double division(double x1, double x2) {
+	
+	double s =  (x1/x2);
+	
+	return s ;
+	
+}
+
+public static double potencia(double x1, double x2) {
+	
+	double s = Math.pow(x1, x2);
+	return s ;
+	
+}
+
+public static double raizCuadrada(double x1) {
+	
+	double s =  (Math.sqrt(x1));
+
+	return s ;
+	
+}
+
+public static double raizcubica(double x1) {
+	
+	double s =  (Math.cbrt(x1));
+	
+	return s ;
+	
+}
+
+
+
+
+
+
+
+
+}
